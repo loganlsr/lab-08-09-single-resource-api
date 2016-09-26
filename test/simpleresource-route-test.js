218583563e7c60343180361ea2e28ed5d20182ed
Logan Rogers <logan.s.rogers@biola.edu>
2016-09-26 10:07:28 -0700

    added another test

--- test/simpleresource-route-test.js
+++ test/simpleresource-route-test.js
@@ -2,6 +2,8 @@
 
 const request = require('superagent');
 const expect = require('chai').expect;
+const storage = require('../lib/storage.js');
+const simpleresource = require('../model/simpleresource.js');
 
 require('../server.js');
 
@@ -14,9 +16,8 @@ describe('testing resource routes', function(){
       .send({name: 'hello', content: 'good bye'})
       .end((err, res) => {
         if (err) return done(err);
-
         expect(res.status).to.equal(200);
-        console.log('res.body', res.body)
+        console.log('res.body', res.body);
         expect(res.body.name).to.equal('hello');
         expect(res.body.content).to.equal('good bye');
         simpleresource = res.body;
@@ -24,8 +25,27 @@ describe('testing resource routes', function(){
       });
     });
   });
-  describe('testing simpleresource routes', function(){
-    describe('testing GET /api/simpleresource', function(){
+
+  describe('testing GET /api/simpleresource', function(){
+    describe('with valid query', function(){
+      before(done => {
+        var simpleresource = {
+          name: 'logan',
+          content: 'good bye',
+        };
+        storage.createItem('simpleresource', simpleresource)
+        .then( simpleresource => {
+          done();
+        })
+        .catch(err => done(err));
+      });
+
+      after(done => {
+        storage.deleteItem('logan', 'good bye')
+        .then(() => done())
+        .catch(err => done(err));
+      });
+
       it('should return a simpleresource', function (done){
         request.get(`localhost:3000/api/simpleresource?id=${simpleresource.id}`)
         .send({name: 'hello', content: 'good bye'})
@@ -36,6 +56,18 @@ describe('testing resource routes', function(){
           expect(res.body.content).to.equal('good bye');
           done();
         });
+      });
+    });
+  });
+
+  describe('testing unregistered route', function(){
+    it('should return the error code 404', function(done){
+      request.get('localhost:3000/api/complexresource')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.text).to.equal('not found');
+        expect(err).to.not.equal.null;
+        done();
       });
     });
   });
